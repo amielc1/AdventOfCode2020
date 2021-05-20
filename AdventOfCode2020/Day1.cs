f56b69faafcc569f4b84cd1b712fd56eb7683018
Amiel Cohen <amielc1@gmail.com>
2021-05-20 23:46:13 +0300

    Add project files.

---
+++ AdventOfCode2020/Day1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2020
+{
+    public class Day1
+    {
+
+        public int  CalcPart1()
+        {
+            int mult = 0;
+            var inputList = getPuzzleInput(@"C:\Users\amielc1\source\repos\AdventOfCode2020\AdventOfCode2020\Day1Input.txt");
+            for (int i = 0; i < inputList.Count; i++)
+            {
+                for (int j = 0; j < inputList.Count; j++)
+                {
+                    var sum = inputList[i] + inputList[j];
+                    if (sum == 2020)
+                    {
+                        mult = inputList[i] * inputList[j];
+                    }
+                }
+            }
+            return mult;
+        }
+        //i-0 j-1 
+        //i-1, j-0 
+
+
+
+
+        private List<int> getPuzzleInput (string puzzleInputFile)
+        {
+            var numbersStr = File.ReadAllLines(puzzleInputFile);
+            var numberInt =  numbersStr.Select(int.Parse).ToList();
+            return numberInt;
+        }
+
+    }
+
+
+
+    
+}
---
+++ AdventOfCode2020/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AdventOfCode2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start!");
+
+            var part1 = new Day1().CalcPart1();
+
+            Console.ReadLine();
+        }
+    }
+}
